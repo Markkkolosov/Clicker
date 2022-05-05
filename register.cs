57b6c92d87e3d31827940ff9c4708ce0d207f113
Markkkolosov <97655760+Markkkolosov@users.noreply.github.com>
2022-05-05 04:19:06 +0300

    Add files via upload
    Инкрементальная игра для курсовой работы

---
+++ register.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class register : MonoBehaviour
+{
+    public void silka()
+    {
+        Application.OpenURL("http://217.71.129.139:4537/");
+    }
+}
